d0b1bf67f3698f761344c156b06e1a0c8554f998
fodorgabor2002 <fodorgabor2002@gmail.com>
2025-12-06 19:18:05 +0100

    modifications for prometheus

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import expressSession  from 'express-session';
+import expressSession from 'express-session';
 import passport from 'passport';
 import { configurePassport } from './passport/passport';
 import mongoose from 'mongoose';
@@ -11,10 +11,46 @@ import { configureUserRoutes } from './routers/usersRouter';
 import { configureTrackRoutes } from './routers/tracksRouter';
 import { configurePlaylistRoutes } from './routers/playlistsRouter';
 import { configureAlbumRoutes } from './routers/albumsRouter';
+import client from 'prom-client';
 
 const app = express();
 const port = 3080;
-const dbURL="mongodb+srv://admin:QKIufoPrObWpWard@cluster.kxduj3p.mongodb.net/Cluster?retryWrites=true&w=majority";
+const dbURL = "mongodb+srv://admin:QKIufoPrObWpWard@cluster.kxduj3p.mongodb.net/Cluster?retryWrites=true&w=majority";
+
+// Prometheus metrikák inicializálása
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+// HTTP request metrikák
+const httpRequestDuration = new client.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'route', 'status_code'],
+    buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10]
+});
+
+const httpRequestTotal = new client.Counter({
+    name: 'http_requests_total',
+    help: 'Total number of HTTP requests',
+    labelNames: ['method', 'route', 'status_code']
+});
+
+register.registerMetric(httpRequestDuration);
+register.registerMetric(httpRequestTotal);
+
+// Middleware a metrikák gyűjtéséhez
+app.use((req: Request, res: Response, next: () => void) => {
+    const start = Date.now();
+
+    res.on('finish', () => {
+        const duration = (Date.now() - start) / 1000;
+        const route = req.route ? req.route.path : req.path;
+        httpRequestDuration.labels(req.method, route, res.statusCode.toString()).observe(duration);
+        httpRequestTotal.labels(req.method, route, res.statusCode.toString()).inc();
+    });
+
+    next();
+});
 
 // mongodb connection
 mongoose.connect(dbURL).then((_) => {
@@ -25,7 +61,7 @@ mongoose.connect(dbURL).then((_) => {
 });
 
 // bodyParser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // cookieParser
 app.use(cookieParser());
@@ -43,8 +79,8 @@ app.use(passport.session());
 
 // CORS beállítás
 app.use(cors({
-  origin: '*',
-  credentials: true, 
+    origin: '*',
+    credentials: true,
 }));
 
 app.use(express.json());
@@ -55,6 +91,12 @@ app.use('/api/user', configureUserRoutes(passport, express.Router()));
 app.use('/api/album', configureAlbumRoutes(passport, express.Router()));
 app.use('/api/track', configureTrackRoutes(passport, express.Router()));
 app.use('/api/playlist', configurePlaylistRoutes(passport, express.Router()));
+
+// Prometheus metrics endpoint
+app.get('/metrics', async (req: Request, res: Response) => {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
 
 app.listen(port, () => {
     console.log('Server is listening on port ' + port.toString());
